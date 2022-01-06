4394a17dc2aae049aa5eafc23f20035b62cdff95
Stadnik Andrii <stadnikandriy1@gmail.com>
2022-01-06 06:44:06 +0200

    WIP

--- simul/python/distance_determination.py
+++ simul/python/distance_determination.py
@@ -1,123 +1,66 @@
+import logging
+
 import numpy as np
-from icecream import ic
 import plotly.express as px
+from icecream import ic
 from tqdm.auto import trange
 
 from Utilities.calc_stear_vect import calc_stear_vect
 from Utilities.generate_point import generate_point
 from Utilities.my_correlation_use import my_correlation_use
+from Utilities.parameters import Parameters
 from Utilities.signals_model import signals_model
 
+logger = logging.getLogger(__name__)
 
-def calc_measure_timestamps():
-    delta_t = 300e-6  # time to do one frequency IQ measurement
-    # times of measurement 8 frequencies in a pack
-    onepack_times = delta_t * np.arange(0.0, 8.0)
-    pack_times = np.arange(0, 7, 2.4e-3).T[:, np.newaxis]  # times of packs measurements staring
-    row, col = len(pack_times), len(onepack_times)
 
-    # pack_times.conj() == pack_times
-    measure_ts = np.tile(onepack_times, (row, 1)) + np.tile(pack_times, (1, col))
+def simulate_signals(p: Parameters):
+    freq_meas_coll = np.full(
+        (p.freq_numb, len(p.measure_timestamps)), np.NaN, np.csingle
+    )
 
-    return measure_ts.flatten()
+    # dist = zeros(min(length(measure_timestamps), length(measure_timestamps)), 4);
+    dist = np.zeros((len(p.measure_timestamps), 4))
+
+    for t_idx in trange(
+        0,
+        len(p.measure_timestamps),
+        desc="Simulating distances and signals",
+        leave=False,
+    ):
+        #  TODO: Write comments about this, I again don't understand how it
+        #  works <06-01-22, astadnik> #
+        if p.freq_set_type == 1:
+            f1_idx = t_idx % len(p.freq_meas_set)
+            f2_idx = np.floor((t_idx) / len(p.freq_meas_set)) % p.f_pack_len
+            curr_freq = p.freq_meas_set[f1_idx] + f2_idx * 2
+        else:
+            curr_freq = float(np.random.uniform(0, p.freq_numb) - 1) * 2
+
+        dist[t_idx, :], ampl_coeff = generate_point(
+            p.start_point_m,
+            p.measure_timestamps[t_idx],
+            p.key_veloc_kmh,
+            p.scenario_matrix,
+            p.scenario_noise,
+        )
+        _, signals = signals_model(
+            curr_freq, dist[t_idx : t_idx + 1, :], ampl_coeff, p.delays, p.noises
+        )
+        freq_meas_coll[:, t_idx] = np.NaN
+        freq_meas_coll[round(curr_freq / 2), t_idx] = signals[0]
+    return dist, freq_meas_coll
 
 
 def main():
     np.random.seed(10)
-    key_veloc_kmh = 5
-    start_point_m = 10
-
-    # scenario_matrix = [0.1, 0, 0, 1]
-    scenario_matrix = [0.2, 0.0, 0.0, 0.6]  # LOS, floor, ceiling, wall
-    # scenario_matrix = [0.2, 0, 0, 0]
-    # scenario_matrix = [0.2, 0.3, 0.4, 0.6]
-    scenario_noise = 0  # not used yet
-
-    freq_numb = 40  # 2MHz step
-
-    freq_list = np.arange(0, freq_numb * 2, 2.0)
-    measure_timestamps = calc_measure_timestamps()
-    # spectr_algo = 1; # 1 - FFT, 2 - MUSIC
-
-    # nsig = 3; # MUSIC ORDER
-    # sm_order = 1; # R matrix smooth level
-    # sampl_dist = 0:0.02:20; # Tested distances
-    # sampl_dist = np.array(np.arange(0, 20, 0.02)) # Tested distances
-
-    freq_set_type = 1
-    # 1 - regular frequency step, 0 - random frequency
-    f_pack_len = 5
-    # for regular frequency step
-    # freq_meas_set = 0:10:70; # for regular frequency step
-    freq_meas_set = np.array(np.arange(0, 80, 10))
-    # for regular frequency step
-
-    ## Define noise and delay errors
-    delays = np.array([])
-    # In this case default numbers will be used
-    noises = np.array([])
-    # In this case default numbers will be used
+    params = Parameters()
 
     ###############################
     # spec_1_evol = zeros(length(measure_timestamps), length(sampl_dist));
     # spec_1_evol = np.zeros((len(measure_timestamps), len(sampl_dist)))
     # freq_meas_coll = NaN(freq_numb, length(measure_timestamps));
-    freq_meas_coll = np.resize(
-        np.array(np.NaN, dtype=np.csingle), (freq_numb, len(measure_timestamps))
-    )
-    print(f"freq_meas_coll:{freq_meas_coll.shape}")
-
-    # dist = zeros(min(length(measure_timestamps), length(measure_timestamps)), 4);
-    dist = np.zeros((max(measure_timestamps.shape), 4))
-    print(f"dist:{dist.shape}")
-    # print(f"    gen_p dist:{dist.shape}")
-
-    ## Determine signals along track
-    # for t_idx = 1:length(measure_timestamps)
-    print(f"    freq_meas_set:{freq_meas_set}")
-
-    for t_idx in trange(0, max(measure_timestamps.shape)):
-        # for t_idx in range(0, 500):
-
-        # for t_idx in range(0,1):
-
-        # if freq_set_type == 1
-        if freq_set_type == 1:
-            #     f1_idx = mod(t_idx - 1, length(freq_meas_set));
-            f1_idx = np.mod(t_idx, len(freq_meas_set))
-            #     f2_idx = mod(floor((t_idx - 1) / length(freq_meas_set)), f_pack_len);
-            f2_idx = np.floor((t_idx) / len(freq_meas_set)) % f_pack_len
-            #     curr_freq = freq_meas_set(f1_idx + 1) + f2_idx * 2;
-            curr_freq = freq_meas_set[f1_idx] + f2_idx * 2
-        # else
-        else:
-            #     curr_freq = (randi(freq_numb) - 1) * 2;
-            curr_freq = float(np.random.uniform(0, freq_numb) - 1) * 2
-
-        # end
-
-        # [dist(t_idx, :), ampl_coeff] = generate_point(start_point_m, measure_timestamps(t_idx), key_veloc_kmh, scenario_matrix, scenario_noise);
-        dist[t_idx, :], ampl_coeff = generate_point(
-            start_point_m,
-            measure_timestamps[t_idx],
-            key_veloc_kmh,
-            scenario_matrix,
-            scenario_noise,
-        )
-        # [~, signals] = signals_model(curr_freq, dist(t_idx, :), ampl_coeff, delays, noises);
-        _, signals = signals_model(
-            curr_freq, dist[t_idx : t_idx + 1, :], ampl_coeff, delays, noises
-        )
-        # if(not PRINT_DIRT):print(f"    signals:{signals.shape}")
-
-        # freq_meas_coll(:, t_idx) = NaN;
-        freq_meas_coll[:, t_idx] = np.NaN
-        # if(not PRINT_DIRT):print(f"    freq_meas_coll:{freq_meas_coll.shape}")
-
-        # freq_meas_coll(round(curr_freq / 2) + 1, t_idx) = signals;
-        freq_meas_coll[round(curr_freq / 2), t_idx] = signals[0]
-        # print(f"        dist:{dist[t_idx, :]}\n    cur_freq:{curr_freq}, signals:{signals[0]}")
-    # end
+    dist, freq_meas_coll = simulate_signals(params)
 
     # data_1 = freq_meas_coll[1, ~np.isnan(freq_meas_coll[1, :])]
     # amplitudes = np.abs(data_1)
@@ -140,7 +83,7 @@ def main():
 
     # Estimate distances
     dists = np.arange(0, 20, 0.02)
-    plot_idxs = np.arange(0, max(measure_timestamps.shape), 8)
+    plot_idxs = np.arange(0, max(params.measure_timestamps.shape), 8)
     dist_probs = np.zeros((max(plot_idxs.shape), max(dists.shape)))
     # for t_idx in trange(0, max(measure_timestamps.shape)):
     for t_idx in trange(0, max(plot_idxs.shape)):
@@ -150,11 +93,12 @@ def main():
         corr_matrix += 1e-10 * np.identity(iq_data.shape[0])
         # VerifyCorrMatrix(corr_matrix)
 
-        stear_vects = calc_stear_vect(freq_list, 2 * dists)
+        stear_vects = calc_stear_vect(params.freq_list, 2 * dists)
         dist_corrs = my_correlation_use(stear_vects, corr_matrix)
         dist_probs[t_idx, :] = (dist_corrs - dist_corrs.min()) / (
             dist_corrs.max() - dist_corrs.min()
         )
+    #  TODO: Plot the ground truth(dist) as well <06-01-22, astadnik> #
     px.imshow(dist_probs, aspect="auto").show()
     # px.
     # print(dist_probs)
